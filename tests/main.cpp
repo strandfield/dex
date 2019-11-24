096669c22edbd7f44eeea4c3cc9491b55e808e57
bandicode <vincent.chambrin@gmail.com>
2019-11-24 11:26:07 +0100

    setup project structure

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2018 Vincent Chambrin
+// This file is part of the Dex project
+// For conditions of distribution and use, see copyright notice in LICENSE
+
+#include <QString>
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
