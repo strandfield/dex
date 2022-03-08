168e6bb6da91a966e8775cb0c98033c46f8f0c25
strandfield <vincent.chambrin@gmail.com>
2022-03-08 19:27:19 +0100

    migrate 'dex-model' to Catch2

--- tests/dex-model/test-dex-model.cpp
+++ tests/dex-model/test-dex-model.cpp
@@ -1,16 +1,16 @@
-// Copyright (C) 2020 Vincent Chambrin
+// Copyright (C) 2020-2022 Vincent Chambrin
 // This file is part of the 'dex' project
 // For conditions of distribution and use, see copyright notice in LICENSE
 
-#include "test-dex-model.h"
-
 #include "dex/model/display-math.h"
 
-void TestDexModel::mathNormalization()
+#include "catch.hpp"
+
+TEST_CASE("Testing math normalization", "[model]")
 {
   std::string src = "\\alpha x + \\gamma \\frac {1}{2}";
 
   dex::DisplayMath::normalize(src, 0);
 
-  QVERIFY(src == "\\alpha x + \\gamma \\frac{1}{2}");
+  REQUIRE(src == "\\alpha x + \\gamma \\frac{1}{2}");
 }
